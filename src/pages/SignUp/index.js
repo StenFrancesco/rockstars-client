1a40a74610a41f88ee6a08bd0229017193451e2f
FaabScheeren <f.scheeren@outlook.com>
2020-04-08 10:55:37 +0200

    Added fields to the signup page with right error messaging

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { signUp } from "../../store/user/actions";
 import { selectToken } from "../../store/user/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
 import { Col } from "react-bootstrap";
+import { Jumbotron } from "react-bootstrap";
+import { setMessage } from "../../store/appState/actions";
 
 export default function SignUp() {
-  const [name, setName] = useState("");
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [street, setStreet] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [houseNumber, setHouseNumber] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
   const history = useHistory();
@@ -24,59 +33,185 @@ export default function SignUp() {
 
   function submitForm(event) {
     event.preventDefault();
+    if (
+      !firstName ||
+      !lastName ||
+      !phone ||
+      !email ||
+      !password ||
+      !street ||
+      !postalCode ||
+      !houseNumber ||
+      !city ||
+      !country
+    ) {
+      dispatch(setMessage("danger", true, "Please fill in all the fields!"));
+    } else {
+      // dispatch(
+      //   signUp(
+      //     firstName,
+      //     lastName,
+      //     phone,
+      //     email,
+      //     password,
+      //     street,
+      //     postalCode,
+      //     houseNumber,
+      //     city,
+      //     country
+      //   )
+      // );
+    }
 
-    dispatch(signUp(name, email, password));
-
+    setfirstName("");
+    setLastName("");
+    setPhone("");
     setEmail("");
     setPassword("");
-    setName("");
+    setStreet("");
+    setPostalCode("");
+    setHouseNumber("");
+    setCity("");
+    setCountry("");
   }
 
   return (
-    <Container>
-      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
-        <h1 className="mt-5 mb-5">Signup</h1>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            value={name}
-            onChange={event => setName(event.target.value)}
-            type="text"
-            placeholder="Enter name"
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-            type="email"
-            placeholder="Enter email"
-            required
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <div>
+      <Jumbotron>
+        <h1>Signup</h1>
+      </Jumbotron>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5 mb-5">
+        <Form.Row>
+          <Form.Group as={Col} controlId="firstName">
+            <Form.Label>Firstname</Form.Label>
+            <Form.Control
+              value={firstName}
+              onChange={(event) => setfirstName(event.target.value)}
+              type="text"
+              placeholder="Enter firstname"
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
+          <Form.Group as={Col} controlId="lastName">
+            <Form.Label>Lastname</Form.Label>
+            <Form.Control
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+              type="text"
+              placeholder="Enter lastname"
+              required
+            />
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="phone">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              value={phone}
+              onChange={(event) => setPhone(event.target.value)}
+              type="text"
+              placeholder="Enter phone"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Group controlId="street">
+          <Form.Label>Street</Form.Label>
+          <Form.Control
+            value={street}
+            onChange={(event) => setStreet(event.target.value)}
+            type="text"
+            placeholder="Street"
+            required
+          />
+        </Form.Group>
+        <Form.Row>
+          <Form.Group as={Col} controlId="postalCode">
+            <Form.Label>Postal code</Form.Label>
+            <Form.Control
+              value={postalCode}
+              onChange={(event) => setPostalCode(event.target.value)}
+              type="text"
+              placeholder="Postal code"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="houseNumber">
+            <Form.Label>House number</Form.Label>
+            <Form.Control
+              value={houseNumber}
+              onChange={(event) => setHouseNumber(event.target.value)}
+              type="text"
+              placeholder="City"
+              required
+            />
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="city">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
+              type="text"
+              placeholder="City"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="country">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              value={country}
+              onChange={(event) => setCountry(event.target.value)}
+              type="text"
+              placeholder="Country"
+              required
+            />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             value={password}
-            onChange={event => setPassword(event.target.value)}
+            onChange={(event) => setPassword(event.target.value)}
             type="password"
             placeholder="Password"
             required
           />
         </Form.Group>
+        <Form.Text className="text-muted">
+          We'll never share your data with anyone else.
+        </Form.Text>
+
         <Form.Group className="mt-5">
+          <Form.Check
+            className="mb-3"
+            required
+            label="Agree to terms and conditions"
+            feedback="You must agree before submitting."
+          />
           <Button variant="primary" type="submit" onClick={submitForm}>
             Sign up
           </Button>
         </Form.Group>
         <Link to="/login">Click here to log in</Link>
       </Form>
-    </Container>
+    </div>
   );
 }
